2ab8b52e67eb130c2cbdb199c33a64583801bbbc
App Academy Student <student@appacademy.io>
2015-11-02 17:28:34 -0800

    write is_valid? method for piece

--- chess/piece.rb
+++ chess/piece.rb
@@ -1,13 +1,24 @@
 class Piece
-  attr_reader :board, :position
+  attr_reader :board, :position, :color
 
-  def initialize(board, position)
+  def initialize(board, position, color)
     @board = board
     @position = position
+    @color = color
   end
 
   def valid_moves
     moves.select { |move| is_valid?(move) }
+  end
+
+  def friendly? other_piece
+    self.color == other_piece.color
+  end
+
+
+  def valid? destination
+    target = board[destination]
+    board.in_bounds?(destination) && !target.friendly?
   end
 
 end
