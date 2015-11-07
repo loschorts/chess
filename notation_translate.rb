f7eeb4f943cd7ddeaee834815e57d3a1c4a662b6
Ken Tran <loschorts@gmail.com>
2015-11-07 12:38:32 -0800

    add notation_translate.rb to convert chess to grid notation

---
+++ notation_translate.rb
@@ -0,0 +1,13 @@
+cm_moves = [%w[f2 f3], %w[e7 e5], %w[g2 g4], %w[d8 h4]]
+
+def translate move
+  positions = move
+  positions.map do |pos|
+    col = ('a'..'h').to_a.find_index(pos.split("")[0])
+    row = 8 - pos.split("")[1].to_i
+    [row, col]
+  end
+  #positions
+end
+
+cm_moves.each {|move| translate move}
